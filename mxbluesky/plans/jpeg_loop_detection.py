177acf07287c08a339edab0447f27362a3d07a05
Michael Skinner <m.skinner5278@gmail.com>
2025-01-17 14:44:40 -0500

    fixed error from empty cleanup

--- mxbluesky/plans/jpeg_loop_detection.py
+++ mxbluesky/plans/jpeg_loop_detection.py
@@ -14,8 +14,8 @@ from logging import getLogger
 logger = getLogger()
 
 
-def clean_up_url_cam():
-    pass
+def clean_up_url_cam(): # TODO: add clean up here
+    return []  # empty list is iterable and prevents error
 
 def trigger_url_camera():
     tries = 0
@@ -58,7 +58,7 @@ def detect_loop(sample_detection: "Dict[str, float|int]"):
     # delta_x = mean_x * 2*two_click_low.pix_per_um.get()
     # delta_cam_y = mean_y * 2*two_click_low.pix_per_um.get()
 
-    omega: float = gonio.omega.get()
+    omega: float = gonio.omega.val()
     sample_detection["face_on_omega"] = omega
 
     #real_y = delta_cam_y * np.cos(np.deg2rad(omega))
@@ -68,34 +68,32 @@ def detect_loop(sample_detection: "Dict[str, float|int]"):
 
     logger.info("Moving sample to center")
     daq_lib.center_on_click(mean_x, mean_y, 0,0, source="unscaled")
+    gonio.task_status().wait(timeout=5)
 
     # The sample has moved to the center of the beam (hopefully), need to update co-ordinates
 
     # orthogonal face, use loop model only if predicted width matches face on
     # otherwise, threshold
-    yield from bps.mv(gonio.omega, sample_detection["face_on_omega"]+90)
+    logger.info(f"rotating omega to: {omega}")
+    #yield from bps.mv(gonio.omega.setpoint, sample_detection["face_on_omega"]+90)
+    gonio.task_status().wait(timeout=5)
+    #yield from bps.wait(gonio.task_status)
     yield from trigger_url_camera()
-
-    try:
-        yield from bp.count([sample_cam], 1)
-    except FailedStatus:
-        yield from bp.count([sample_cam], 1)
-    return
-'''
+    '''
+    logger.info(f"second image triggered")
     loop_detector.get_threshold.set(True)
     loop_detector.x_start.set(int(box_coords_face[0]-mean_x))
     loop_detector.x_end.set(int(box_coords_face[2]-mean_x))
-    loop_detector.filename.set(two_click_low.jpeg.full_file_name.get())
-    
+    loop_detector.filename.set(sample_cam.filename.get())
+    logger.info(f"calling loop detector again")
     scan_uid = yield from bp.count([loop_detector], 1)
     box_coords_ortho = db[scan_uid].table()['loop_detector_box'][1]
     box_coords_ortho_threshold = db[scan_uid].table()['loop_detector_thresholded_box'][1]
     mean_y_threshold = (box_coords_ortho_threshold[1] + box_coords_ortho_threshold[3]) / 2
-    small_box_height_threshold = (box_coords_ortho_threshold[3] - box_coords_ortho_threshold[1]) * 2 * two_click_low.pix_per_um.get()
-
+    small_box_height_threshold = (box_coords_ortho_threshold[3] - box_coords_ortho_threshold[1]) * 2 / camera.scale_x.get() / 1000
     try:
         mean_y = (box_coords_ortho[1] + box_coords_ortho[3]) / 2
-        sample_detection["small_box_height"] = (box_coords_ortho[3] - box_coords_ortho[1]) * 2 * two_click_low.pix_per_um.get()
+        sample_detection["small_box_height"] = (box_coords_ortho[3] - box_coords_ortho[1]) * 2 / camera.scale_y.get() /1000
                 # sum of squared difference, face-on vs. ortho width similarity
         ssd_ratio = (
             ((box_coords_face[0]-mean_x - box_coords_ortho[0])**2 +
