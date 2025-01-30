d6a4c6bb73613fc2060997c3e22dbe3002fd3a40
mskinner5278 <m.skinner5278@gmail.com>
2025-01-30 15:18:25 -0500

    fixed det dist metadata

--- daq_macros.py
+++ daq_macros.py
@@ -3915,10 +3915,11 @@ def rasterDaq(rasterReqID):
         logger.warning("Detector was in the armed state prior to this attempted collection.")
         return 0
     start_time = time.time()
+    detDistM = detDist / 1000 # det metadata wants meters
     raster_flyer.configure_detector(rasterFilePrefix, data_directory_name)
     raster_flyer.detector_arm(start_omega, img_width_per_cell, frames_per_line, exposure_per_image, 
                      file_prefix, data_directory_name, file_number_start, xbeam, ybeam, 
-                     wavelength, detDist, num_triggers=number_of_lines)
+                     wavelength, detDistM, num_triggers=number_of_lines)
     def armed_callback(value, old_value, **kwargs):
         return (old_value == 0 and value == 1)
     arm_status = SubscriptionStatus(raster_flyer.detector.cam.armed, armed_callback, run=False)
