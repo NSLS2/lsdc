bd93dab36b69a1438186b659507ce1ecad1593ae
vshekar1 <vshekar1@bnl.gov>
2025-10-19 17:29:22 -0400

    Changed LSDC server to use PV callbacks

--- start_bs.py
+++ start_bs.py
@@ -41,7 +41,7 @@ plt.ion()
 import bluesky.plans as bp
 
 from bluesky.run_engine import RunEngine
-RE = RunEngine()
+RE = RunEngine(context_managers=[])
 beamline = os.environ["BEAMLINE_ID"]
 from databroker import Broker
 db = Broker.named(beamline)
