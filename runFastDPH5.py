bbbbb33ea0a2ba435556fddb6039de871df2a906
vshekar1 <vshekar1@bnl.gov>
2026-01-12 13:11:26 -0500

    Updated runFastDP to handle failure in fastdp execution

--- runFastDPH5.py
+++ runFastDPH5.py
@@ -4,6 +4,7 @@ import sys
 import db_lib
 from daq_utils import getBlConfig, setBlConfig
 import xmltodict
+import json
 import logging
 import subprocess
 logger = logging.getLogger()
@@ -36,30 +37,40 @@ ispybDCID = 1 #int(sys.argv[8])
 runAutoProc = int(sys.argv[9])
 # runAutoProc = 0
 
-comm_s = f"ssh -q {node} \"{os.environ['MXPROCESSINGSCRIPTSDIR']}fast_dp.sh {request_id} {numstart}\""
-logger.info(comm_s)
-os.system(comm_s)
+try:
+  comm_s = f"ssh -q {node} \"{os.environ['MXPROCESSINGSCRIPTSDIR']}fast_dp.sh {request_id} {numstart}\""
+  logger.info(comm_s)
+  os.system(comm_s)
+  fastDPResultFile = runningDir+"/fast_dp.xml"
+  fd = open(fastDPResultFile)
+  resultObj = xmltodict.parse(fd.read())
+  logger.info(f"finished fast_dp {request_id}")
+  resultID = db_lib.addResultforRequest("fastDP",request_id,owner,resultObj,beamline=os.environ["BEAMLINE_ID"])
+  newResult = db_lib.getResult(resultID)
+  visitName = getBlConfig("visitName")
+except Exception as e:
+  logger.error("runfastdph5 error running fastdp: %s" % e)
 
-fastDPResultFile = runningDir+"/fast_dp.xml"
-fd = open(fastDPResultFile)
-resultObj = xmltodict.parse(fd.read())
-logger.info(f"finished fast_dp {request_id}")
-resultID = db_lib.addResultforRequest("fastDP",request_id,owner,resultObj,beamline=os.environ["BEAMLINE_ID"])
-newResult = db_lib.getResult(resultID)
-visitName = getBlConfig("visitName")
 try:
   ispybLib.insertResult(newResult,"fastDP",request,visitName,ispybDCID,fastDPResultFile)
 except Exception as e:
   logger.error("runfastdph5 insert result ispyb error: %s" % e)
+
 if (runFastEP):
   os.system("fast_ep") #looks very bad! running on ca1!
-if (runDimple):
-  dimpleComm = getBlConfig("dimpleComm")
-  comm_s = f"ssh -q {dimpleNode} \"{os.environ['MXPROCESSINGSCRIPTSDIR']}dimple.sh {request_id} {numstart}\""  
-  logger.info(f"running dimple: {comm_s}")
-  os.system(comm_s)
 
+if (runDimple):
+  try:
+    dimpleComm = getBlConfig("dimpleComm")
+    comm_s = f"ssh -q {dimpleNode} \"{os.environ['MXPROCESSINGSCRIPTSDIR']}dimple.sh {request_id} {numstart}\""  
+    logger.info(f"running dimple: {comm_s}")
+    # os.system(comm_s)
+    subprocess.Popen(comm_s, shell=True)
+  except Exception as e:
+    logger.error("runfastdph5 error running dimple: %s" % e)
+    
 if runAutoProc:
+  logger.info("Running AUTO-AUTOPROC...")
   setBlConfig("auto_proc_lock", True)
   try:
     queue = getBlConfig("auto_proc_queue")
@@ -70,7 +81,8 @@ if runAutoProc:
   finally:
     setBlConfig("auto_proc_lock", False)
   start_in_proc = None
-  for proc_num in ['uranus-cpu044', 'uranus-cpu041', 'uranus-cpu021']:
+  autoproc_processor_list = json.loads(getBlConfig("autoprocNodes"))
+  for proc_num in autoproc_processor_list:
     if not getBlConfig(proc_num):
       start_in_proc = proc_num
       break
